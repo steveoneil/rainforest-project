ba0c5e08e692f8e6c6631b33bb46a37ca6f8a476
steveoneil <steve@steveoneil.com>
2016-02-01 14:29:01 -0500

    Writing product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
 
@@ -6,4 +7,5 @@ class Product < ActiveRecord::Base
     price_in_dollars = price_in_cents.to_f / 100
     sprintf("%.2f", price_in_dollars)
   end
+  
 end
